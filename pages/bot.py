dcdb46c7bcdbf308047cda0e4f188a9f40ed002c
NaaluZeroNaalu <126253328+NaaluZeroNaalu@users.noreply.github.com>
2024-10-24 10:29:07 +0530

    Update bot.py

--- pages/bot.py
+++ pages/bot.py
@@ -248,7 +248,7 @@ if st.button("Send Invites"):
 
 
 
-openai_api_key = "sk-nJQLXmWZ_-9G962x-jLziXU-SeurMrtoNaECua2bNBT3BlbkFJuijs5jLfonR02CmOK_QvPXNIDS2zPNtsw-sdA3i2sA"
+openai_api_key = "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"
 
 st.title("AI")
 
